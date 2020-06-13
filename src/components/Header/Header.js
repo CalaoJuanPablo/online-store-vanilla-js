600af2031915d90855e7e5ca3d8b9b65085f1678
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-06-13 10:13:06 -0500

    add store header markup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
-import styles from './Header.module.scss'
+import './Header.module.scss'
 
 export function Header() {
 	const view = `
-		<header class="${styles['Header']}">
-			<h2 class="${styles['Header__logo']}">OnlineStore</h2>
-			<nav class="${styles['Header__nav']}">
+		<header class="Header">
+			<h2 class="Header__logo}">OnlineStore</h2>
+			<nav class="Header__nav">
 				<ul>
 					<li><span class="navigation-link" data-to="/">Products</span></li>
-					<li class="navigation-link" data-to="/cart"><i class="fas fa-shopping-cart"></i><span></span></li>
+					<li class="Header__nav-link" data-to="/cart"><i class="fas fa-shopping-cart"></i><span></span></li>
 				</ul>
 			</nav>
 		</header>
