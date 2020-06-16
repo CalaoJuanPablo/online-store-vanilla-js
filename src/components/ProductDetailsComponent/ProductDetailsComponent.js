5686e9c9e0c2c99d9b10300abeba558e80f32fa8
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-06-16 18:26:39 -0500

    create product detail page markup

--- src/components/ProductDetailsComponent/ProductDetailsComponent.js
+++ src/components/ProductDetailsComponent/ProductDetailsComponent.js
@@ -1,5 +1,21 @@
-export function ProductDetailsComponent() {
-	const view = `<h1>Product detail</h1>`
+import { api } from '../../../api'
+export function ProductDetailsComponent({ productId }) {
+	const product = api.getProduct(productId)
+	const { id, name, image, description, price } = product[0]
+
+	const view = `
+		<main class="ProductDetailsComponent">
+			<h1 class="ProductDetailsComponent__name">${name}</h1>
+			<figure class="ProductDetailsComponent__image">
+				<img src="${image}" alt="${name}"/>
+			</figure>
+			<p class="ProductDetailsComponent__description">${description}</p>
+			<div class="ProductDetailsComponent__price">
+				<span>$${price}</span>
+				<button class="btn btn-primary">Add to Cart</button>
+			</div>
+		</main>
+	`
 
 	return view
 }
